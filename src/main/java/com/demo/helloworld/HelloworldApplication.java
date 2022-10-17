b2d2117efb83a37733e1dd4eb2a8d9fdecc88cdd
Tài Trương <giaptai170@gmail.com>
2022-10-17 14:13:58 +0700

    first commit

---
+++ src/main/java/com/demo/helloworld/HelloworldApplication.java
@@ -0,0 +1,21 @@
+package com.demo.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloworldApplication {
+
+	@GetMapping("/")
+	String home() {
+		return "Hello Java team";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
