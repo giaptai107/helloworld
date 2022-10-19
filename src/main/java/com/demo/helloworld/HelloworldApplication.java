8403007d75e8b062e85e715ab852e2528dbb95f1
gita107 <55130393+giaptai107@users.noreply.github.com>
2022-10-19 14:31:51 +0700

    Update HelloworldApplication.java

--- src/main/java/com/demo/helloworld/HelloworldApplication.java
+++ src/main/java/com/demo/helloworld/HelloworldApplication.java
@@ -11,7 +11,7 @@ public class HelloworldApplication {
 
 	@GetMapping("/")
 	String home() {
-		return "Hello Java team STDIO Huế lần 7";
+		return "Hello Java team STDIO Huế lần 8";
 	}
 
 	public static void main(String[] args) {
